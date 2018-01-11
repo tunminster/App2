5a15b4c4c1f4c7212b0cd5a2d36bfc5e202a8cdd
Tun Hein <tun.hein@harveynichols.com>
2018-01-11 16:15:14 +0000

    Add project files.

---
+++ App2/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace App2
+{
+    [Activity(Label = "App2", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+
+            // Get UI Controls
+            EditText phoneNumberText = FindViewById<EditText>(Resource.Id.etPhoneNumber);
+            TextView translatedPhoneWord = FindViewById<TextView>(Resource.Id.tvTranslatedPhoneWord);
+            Button translateButton = FindViewById<Button>(Resource.Id.btnTranslate);
+
+            // Add code to translate number
+            translateButton.Click += (sender, e) =>
+            {
+                // Translate user’s alphanumeric phone number to numeric
+                string translatedNumber = Core.PhonewordTranslator.ToNumber(phoneNumberText.Text);
+                if (string.IsNullOrWhiteSpace(translatedNumber))
+                {
+                    translatedPhoneWord.Text = string.Empty;
+                }
+                else
+                {
+                    translatedPhoneWord.Text = translatedNumber;
+                }
+            };
+        }
+    }
+}
+
